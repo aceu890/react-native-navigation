896ad3a2976b3c77d21c04755e73791b9f708f84
Fernando <eldersoto1876@hotmail.com>
2023-02-07 18:04:23 -0600

    navegar a otras pantallas

--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
 
 export const Pagina2Screen = () => {
+  const navigator = useNavigation<any>();
+
   return (
-    <View>
-        <Text>Pagina2Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text>Pagina2Screen 😎</Text>
+
+      <Button
+        title="Ir página 3"
+        onPress={ () => navigator.navigate('Pagina3Screen') }
+      />
     </View>
   );
 };
--- src/screens/Pagina3Screen.tsx
+++ src/screens/Pagina3Screen.tsx
@@ -1,10 +1,24 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
 
-export const Pagina3Screen = () => {
+interface Props extends StackScreenProps<any, any>{};
+
+export const Pagina3Screen = ( { navigation }: Props) => {
   return (
-    <View>
+    <View style={ styles.globalMargin}>
         <Text>Pagina3Screen</Text>
+
+        <Button 
+          title="Regresar"
+          onPress={ () => navigation.pop() }
+        />
+
+        <Button 
+          title="Ir a página 1"
+          onPress={ () => navigation.popToTop() }
+        />
     </View>
   );
 };
