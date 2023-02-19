c1ef43bff5f27d0fc39daeb0a435709dec23fa00
Fernando <eldersoto1876@hotmail.com>
2023-02-18 18:44:53 -0600

    106 end - estilizando el stack navigator

--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,14 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 export const Pagina2Screen = () => {
   const navigator = useNavigation<any>();
 
+  useEffect(() => {
+    navigator.setOptions({
+      title: 'Hola Mundo',
+      headerBackTitle: 'Atrás'
+    })
+  }, [])
+  
   return (
     <View style={styles.globalMargin}>
-      <Text>Pagina2Screen 😎</Text>
+      <Text style= {styles.title}>Pagina2Screen 😎</Text>
 
       <Button
         title="Ir página 3"
