39ef2cc3c0bcab2d7dd5cbdd43871ca8c0412b73
Fernando <eldersoto1876@hotmail.com>
2023-02-21 02:29:47 -0600

    111-configurar drawer basico

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { Pagina1Screen } from '../screens/Pagina1Screen';
 import { Pagina2Screen } from '../screens/Pagina2Screen';
