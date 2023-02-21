3e357d669779c7dd79102a6cd6dccb60abb8124a
Fernando <eldersoto1876@hotmail.com>
2023-02-21 01:32:24 -0600

    108-diferente forma de colocar el tipo de dato de los argumentos

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -3,30 +3,48 @@ import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { styles } from '../theme/appTheme';
 
-interface Props extends StackScreenProps<any, any>{};
+interface Props extends StackScreenProps<any, any> { };
 
 
-export const Pagina1Screen = ( { navigation }: Props ) => {
+export const Pagina1Screen = ({ navigation }: Props) => {
 
   return (
-    <View style={ styles.globalMargin }>
-        <Text style= {styles.title}>Pagina1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Pagina1Screen</Text>
 
-        <Button 
-          title="Ir página 2"
-          onPress={ () => navigation.navigate('Pagina2Screen') }
-        />
+      <Button
+        title="Ir página 2"
+        onPress={() => navigation.navigate('Pagina2Screen')}
+      />
 
       <Text>Navegar con argumentos</Text>
 
-      <TouchableOpacity
-        onPress={ () => navigation.navigate('PersonaScreen', {
-          id: 1,
-          nombre: 'pedro'
-        }) }
-      >
-        <Text>Pedro</Text>
-      </TouchableOpacity>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={{
+            ...styles.botonGrande,
+            backgroundColor: '#5856D6'
+          }}
+          onPress={() => navigation.navigate('PersonaScreen', {
+            id: 1,
+            nombre: 'pedro'
+          })}
+        >
+          <Text style={styles.botonGrandeTexto}>Pedro</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            ...styles.botonGrande,
+            backgroundColor: '#FF9427'
+          }}
+          onPress={() => navigation.navigate('PersonaScreen', {
+            id: 2,
+            nombre: 'Maria'
+          })}
+        >
+          <Text style={styles.botonGrandeTexto}>Maria</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
